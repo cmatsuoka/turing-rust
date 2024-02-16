7bee10c46527afdf07e4a11d4a6bd5482b713c5d
Claudio Matsuoka <cmatsuoka@gmail.com>
2024-02-16 13:39:29 -0300

    cli: add a command line parser
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use crate::cpu::CpuLoad;
 use crate::scheduler::{Scheduler, Task};
 use crate::screen::ScreenRevA;
 use crate::themes::load;
+use clap::Parser;
 use simple_logger::SimpleLogger;
-use std::env;
+use std::error::Error;
+use std::process;
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
@@ -14,20 +16,34 @@ mod scheduler;
 mod screen;
 mod themes;
 
-fn main() {
-    // TODO: parse command line
-    let args: Vec<String> = env::args().collect();
-    let name = &args[1];
+#[derive(Parser)]
+struct Args {
+    /// Screen brightness
+    #[arg(short, long)]
+    brightness: Option<i32>,
 
+    theme: String,
+}
+
+fn main() {
     SimpleLogger::new().init().unwrap();
 
-    let theme = load(name).unwrap();
+    let args = Args::parse();
 
-    let mut cpu_percent_collector = psutil::cpu::CpuPercentCollector::new().unwrap();
-    let cpu_percent = cpu_percent_collector.cpu_percent().unwrap();
+    match run(args) {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("error: {err}");
+            process::exit(1);
+        }
+    }
+}
 
-    println!("{}", cpu_percent);
-    println!("{:?}", theme);
+fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    let theme_name = args.theme;
+    let theme = load(&theme_name)?;
+
+    log::info!("using theme: {:?}", theme_name);
 
     let _scr = ScreenRevA::new("AUTO");
     let (tx, rx) = mpsc::channel();
